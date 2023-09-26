91a04a0457cc775f8aba79441d9bee405f28ddc7
yyczz <a729746954@gmail.com>
2023-09-26 08:48:35 +0800

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,33 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+import { WagmiConfig, chain, configureChains, createClient } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { mainnet, polygon, optimism, arbitrum, base, zora } from "wagmi/chains";
+import { RainbowKitProvider, getDefaultWallets } from "@rainbow-me/rainbowkit";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon],
+  [publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "xxx",
+  chains,
+});
+
+const wagmiClinet = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClinet}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }

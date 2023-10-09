5b34f8eada54c3e5fce1a3e3d365d9e43f210e92
zerooooooo1 <xushiduo1@gmail.com>
2023-10-09 11:18:46 +0800

    新的分支

--- components/bar/NavBar.js
+++ components/bar/NavBar.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
-
 import Link from 'next/link'
 
 const NavBar = () => {
     return (
         <div className="navbar p-4">
             <div className="navbar-start">
-                <a className="btn btn-ghost normal-case text-xl">EZSWAP</a>
+                <img src='/logo.svg' />
             </div>
             <div className="navbar-center">
+                <a class="header-btn search-btn launchpad">Search</a>
+                <a class="header-btn search-btn launchpad">Launchpad</a>
+                <a class="header-btn search-btn launchpad">API</a>
+                <a class="header-btn search-btn launchpad">Buy/Sell Crypto</a>
                 <ul className="menu menu-horizontal px-1">
                     <li>
                         <details>

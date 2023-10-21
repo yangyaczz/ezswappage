956493dca268e6284e18dbfbbaac811319bfb757
zerooooooo1 <xushiduo1@gmail.com>
2023-10-21 09:41:29 +0800

    增加首页路由

--- config/constant.js
+++ config/constant.js
@@ -3,5 +3,5 @@ import nextConfig from '../next.config.js'
 export const BASE_URL = {
   dev: 'https://testapi.ezswap.io/api/',
   test: 'https://testapi.ezswap.io/api/',
-  prod: 'https://testapi.ezswap.io/api/',
+  prod: 'https://api.ezswap.io/api/',
 }[nextConfig.publicRuntimeConfig.env.API];

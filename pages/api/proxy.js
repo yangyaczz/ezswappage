fd7f235f4ae6a95eb268df02f9f6149349418545
yyczz <a729746954@gmail.com>
2023-11-23 00:36:12 +0800

    fix: fix proxy

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -2,33 +2,33 @@ import axios from "axios";
 import { BASE_URL } from "../../config/constant";
 import nextConfig from "../../next.config.js";
 
-// export default async function handler(req, res) {
-//   try {
-//     const response = await axios.post(
-//       BASE_URL + "api/queryCollectionPoolList",
-//       req.body
-//     );
-//     console.log(response);
-//     res.status(200).json(response.data);
-//   } catch (error) {
-//     res.status(error.response?.status || 500).json(error.response?.data || {});
-//   }
-// }
-
-export default async function handler(body) {
-  const res = { ok: false, status: null, data: {} };
+export default async function handler(req, res) {
   try {
     const response = await axios.post(
       BASE_URL + "api/queryCollectionPoolList",
-      body
+      req.body
     );
-    res.status = response.status;
-    res.data = response.data.data;
-    res.ok = true;
+    console.log(response);
+    res.status(200).json(response.data);
   } catch (error) {
-    res.status = error.status;
-    res.ok = false;
+    res.status(error.response?.status || 500).json(error.response?.data || {});
   }
-
-  return res;
 }
+
+// export default async function handler(body) {
+//   const res = { ok: false, status: null, data: {} };
+//   try {
+//     const response = await axios.post(
+//       BASE_URL + "api/queryCollectionPoolList",
+//       body
+//     );
+//     res.status = response.status;
+//     res.data = response.data.data;
+//     res.ok = true;
+//   } catch (error) {
+//     res.status = error.status;
+//     res.ok = false;
+//   }
+
+//   return res;
+// }

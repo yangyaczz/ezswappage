aa4fc7ff4d4d339679d3ebf61d7ba9ceb2289dc0
LLHOYH <llhoyh01@gmail.com>
2024-01-07 17:17:13 +0800

    change to 'claims ended in', added multiple languages

--- components/airdropclaim/Countdown.js
+++ components/airdropclaim/Countdown.js
@@ -1,34 +1,44 @@
 import React, { use, useEffect, useState } from "react";
 import TimerBox from "./TimerBox";
+import { useLanguage } from "@/contexts/LanguageContext";
 
-const Countdown = ({setClaimAvailable}) => {
-    const liveTime = new Date(2024, 1, 1, 12, 30, 0, 0); //2024 1st Feb 12.30
-//   const liveTime = new Date(2024, 0, 7, 2, 7, 0, 0); //2024 1st Feb 12.30
+const Countdown = ({ setClaimAvailable }) => {
+  // const claimStartTimestamp = 1704514565;
+
+  const {languageModel} = useLanguage();
+
+
+  const claimEndTimestamp = 1704687365;
+  const claimEndTime = new Date(claimEndTimestamp * 1000); //convert to miliseconds
+
+  // const dummyTime = new Date(2024, 0, 8, 14, 34, 0, 0);
+  // console.log(dummyTime.getTime() / 1000)
+  // const dummyStartTime = 1704695640 //new Date(2024, 1, 1, 12, 30, 0, 0);
+  // const claimEndTime = new Date(dummyStartTime * 1000)//convert to miliseconds
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft);
   function calculateTimeLeft() {
     const nowTime = new Date(new Date() + 60000);
-    let diffTime = liveTime - nowTime;
-    if (diffTime <= 0){
-
-        return {
-            expire: true,
-            days: "00",
-            hours: "00",
-            minutes: "00",
-            seconds: "00",
-        };
+    let diffTime = claimEndTime - nowTime;
+    if (diffTime <= 0) {
+      return {
+        expire: true,
+        days: "00",
+        hours: "00",
+        minutes: "00",
+        seconds: "00",
+      };
     }
-    var days = String(Math.floor(diffTime / (1000 * 60 * 60 * 24))).padStart(
+    let days = String(Math.floor(diffTime / (1000 * 60 * 60 * 24))).padStart(
       2,
       "0"
     );
-    var hours = String(
+    let hours = String(
       Math.floor((diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     ).padStart(2, "0");
-    var minutes = String(
-      Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60) + 1) // need to plus 1, just for display. Won't affect the calculating logic
+    let minutes = String(
+      Math.floor((diffTime % (1000 * 60 * 60)) / (1000 * 60))
     ).padStart(2, "0");
-    var seconds = String(Math.floor((diffTime % (1000 * 60)) / 1000)).padStart(
+    let seconds = String(Math.floor((diffTime % (1000 * 60)) / 1000)).padStart(
       2,
       "0"
     );
@@ -39,10 +49,10 @@ const Countdown = ({setClaimAvailable}) => {
   useEffect(() => {
     const countdownInterval = setInterval(() => {
       let timeLeft = calculateTimeLeft();
-      setTimeLeft(calculateTimeLeft())
+      setTimeLeft(calculateTimeLeft());
       if (timeLeft.expire) {
         clearInterval(countdownInterval);
-        setClaimAvailable(true);
+        setClaimAvailable(false);
       }
     }, 1000);
 
@@ -52,21 +62,38 @@ const Countdown = ({setClaimAvailable}) => {
   return (
     <div id="countdown" className="flex items-center justify-center text-2xl">
       <label className="hidden text-xs whitespace-nowrap md:block md:text-lg lg:text-2xl xl:text-3xl">
-        Claiming Live In:
+        {languageModel.ClaimingEndsIn}:
       </label>
-      <TimerBox value={timeLeft.days.toString().substring(0, 1)} />
-      <TimerBox value={timeLeft.days.toString().substring(1, 2)} />
-      <label className="text-xs text-white xl:text-base lg:text-sm">Days</label>
+      {timeLeft.days > 0 && (
+        <>
+          <TimerBox value={timeLeft.days.toString().substring(0, 1)} />
+          <TimerBox value={timeLeft.days.toString().substring(1, 2)} />
+          <label className="text-xs text-white xl:text-base lg:text-sm">
+            {languageModel.Days}
+          </label>
+        </>
+      )}
+
       <TimerBox value={timeLeft.hours.toString().substring(0, 1)} />
       <TimerBox value={timeLeft.hours.toString().substring(1, 2)} />
       <label className="text-xs text-white xl:text-base lg:text-sm">
-        Hours
+        {languageModel.Hours}
       </label>
       <TimerBox value={timeLeft.minutes.toString().substring(0, 1)} />
       <TimerBox value={timeLeft.minutes.toString().substring(1, 2)} />
       <label className="text-xs text-white xl:text-base lg:text-sm">
-        Minutes
+        {languageModel.Minutes}
       </label>
+
+      {timeLeft.days <= 0 && (
+        <>
+          <TimerBox value={timeLeft.seconds.toString().substring(0, 1)} />
+          <TimerBox value={timeLeft.seconds.toString().substring(1, 2)} />
+          <label className="text-xs text-white xl:text-base lg:text-sm">
+            {languageModel.Seconds}
+          </label>
+        </>
+      )}
     </div>
   );
 };
--- pages/airdropclaim/index.js
+++ pages/airdropclaim/index.js
@@ -3,22 +3,25 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState, useEffect } from "react";
 import styles from "./index.module.scss";
 import Countdown from "@/components/airdropclaim/Countdown";
+import { useLanguage } from "@/contexts/LanguageContext";
 
 const AirdropClaim = () => {
-  const [claimAvailable, setClaimAvailable]= useState(false);
+  const [claimAvailable, setClaimAvailable] = useState(true);
+  const [claimStatus, setClaimStatus] = useState("ELIGIBLE"); //ELIGIBLE, INELIGIBLE, CLAIMED, ENDED
+  const { languageModel } = useLanguage();
 
-  function handleClaimClick(){
-    if(!claimAvailable) return;
+  function handleClaimClick() {
+    if (!claimAvailable) return;
 
     //logic here
   }
-  
+
   return (
-    <div className={"w-full text-[#00D5DA] " + styles.divBackground} >
+    <div className={"w-full text-[#00D5DA] " + styles.divBackground}>
       <div className="grid w-5/6 h-full grid-cols-1 grid-rows-[2fr,3fr,9fr] sm:grid-rows-[2fr,3fr,15fr] m-auto">
         <button className="self-center border-solid border-[1px] border-[#00D5DA] w-28 sm:w-40 h-8 rounded font-thin py-1">
           <FontAwesomeIcon icon={faAnglesLeft} />
-          Back
+          {languageModel.Back}
         </button>
         <section
           id="header"
@@ -26,22 +29,57 @@ const AirdropClaim = () => {
         >
           <div className="flex flex-row items-center justify-start gap-3">
             <img src="/ezicon.svg" alt="logo" />
-            <p className="text-4xl font-extrabold whitespace-nowrap md:text-xl lg:text-2xl xl:text-4xl">EZswap Airdrop</p>
+            <p className="text-4xl font-extrabold whitespace-nowrap md:text-xl lg:text-2xl xl:text-4xl">
+              EZswap {languageModel.Airdrop}
+            </p>
           </div>
-          <Countdown setClaimAvailable={setClaimAvailable}/>
-
+          <Countdown setClaimAvailable={setClaimAvailable} />
         </section>
         <section
           id="claiming-section"
           className="flex flex-col items-center justify-start text-5xl font-black gap-y-11 "
         >
-          <h1 className="text-2xl mt-11 sm:text-5xl">You are eligible for:</h1>
-          <h1>
-            <span className="text-white ">999&nbsp;</span>
-            $EZ
-          </h1>
-          <button className={`text-black text-lg w-32 rounded-3xl font-bold px-2 py-1 ${claimAvailable?"bg-[#00D5DA]":"bg-gray-500"}`} disabled={!claimAvailable} onClick={handleClaimClick}>
-            Claim
+          {claimStatus === "ELIGIBLE" && (
+            <>
+              <h1 className="text-2xl mt-14 sm:text-5xl">
+                {languageModel.YouAreEligibleFor}:
+              </h1>
+              <h1>
+                <span className="text-white ">999&nbsp;</span>
+                $EZ
+              </h1>
+            </>
+          )}
+          {claimStatus === "CLAIMED" && (
+            <>
+              <h1 className="mt-20 text-2xl sm:text-5xl">
+                {languageModel.YouHaveClaimed}
+              </h1>
+              <h1>
+                <span className="text-white ">999&nbsp;</span>
+                $EZ
+              </h1>
+            </>
+          )}
+          {claimStatus === "INELIGIBLE" && (
+            <h1 className="mt-48 text-2xl sm:text-5xl">
+              {languageModel.SorryYouAreNotEligible}
+            </h1>
+          )}
+          {claimStatus === "ENDED" && (
+            <h1 className="mt-48 text-2xl sm:text-5xl">
+              {languageModel.SorryAirdropEnded}
+            </h1>
+          )}
+
+          <button
+            className={`text-black text-lg w-32 rounded-3xl font-bold px-2 py-1 ${
+              claimStatus === "ELIGIBLE" ? "block" : "hidden"
+            } ${claimAvailable ? "bg-[#00D5DA]" : "bg-gray-500"}`}
+            disabled={!claimAvailable}
+            onClick={handleClaimClick}
+          >
+            {languageModel.Claim}
           </button>
         </section>
       </div>

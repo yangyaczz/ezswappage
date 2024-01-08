c235bc419dfbcf6495bb4674447db9e8de19a9bc
zerooooooo1 <xushiduo1@gmail.com>
2024-01-08 22:11:07 +0800

    同步先用测试环境

--- pages/api/proxy.js
+++ pages/api/proxy.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   try {
     //aaaa
     const response = await axios.post(
-      BASE_URL + "api/queryCollectionPoolList",
+      "https://testapi.ezswap.io/" + "api/queryCollectionPoolList",
       req.body
     );
     res.status(200).json(response.data);

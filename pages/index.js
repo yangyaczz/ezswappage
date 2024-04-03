8eb6ae7611dcf2e127106d6f541c9bbdf2b2811b
zerooooooo1 <xushiduo1@gmail.com>
2024-04-04 02:15:13 +0800

    修改文案

--- pages/index.js
+++ pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
     return (
         <div className="min-[1700px]:flex min-[1700px]:flex-col min-[1700px]:items-center">
             <div className={"flex flex-col items-center " + styles.divBackground}>
-                <div className="min-[1500px]:ml-20  ml-14 min-[1700px]:ml-32  max-[800px]:ml-0">
+                <div className="max-[1250px]:ml-10 min-[1500px]:ml-20  ml-14 min-[1700px]:ml-32  max-[800px]:ml-0">
                     {screenWidth > 800 ?
                         // web
                         <div className="flex items-center mt-20 backdrop-blur-sm">
@@ -75,13 +75,13 @@ export default function Home() {
                         </div>
                         <div className="flex max-[800px]:flex-col max-[800px]:mr-10">
                             {/*第一个item*/}
-                            <div className="bg-black border border-[#737373] rounded-md pt-10 pb-7 max-[800px]:pb-4 text-white min-[799px]:mr-5 max-[1500px]:h-[240px] max-[1300px]:w-[380px] max-[1500px]:w-[400px] w-[480px]">
+                            <div className="bg-black border border-[#737373] rounded-md pt-10 pb-7 max-[800px]:pb-4 text-white min-[799px]:mr-5 max-[1500px]:h-[240px] max-[1250px]:w-[330px] max-[1300px]:w-[380px] max-[1500px]:w-[400px] max-[1700px]:w-[440px] w-[480px]">
                                 {/*最外面上下布局*/}
                                 <div className="flex items-center pl-8 pr-5 max-[800px]:pr-1 max-[800px]:pl-5">
                                     {/*里面左右布局*/}
                                     <img className="w-[50px]" src="/logo256.png" alt=""/>
                                     <div className="ml-8 max-[800px]:ml-5">
-                                        <div className="text-4xl max-[1500px]:text-3xl font-bold max-[800px]:text-3xl">EZswap Protocol</div>
+                                        <div className="text-4xl max-[1500px]:text-3xl font-bold max-[800px]:text-3xl max-[1250px]:text-2xl">EZswap Protocol</div>
                                         <div className="text-sm mt-1 max-[800px]:text-xs">The Premier Game Assets and Meme DEX Protocol</div>
                                     </div>
                                 </div>
@@ -92,51 +92,51 @@ export default function Home() {
                                         <a href="https://www.bitget.com/zh-CN/spot/EZSWAPUSDT" className="flex items-center underline mt-2"><img className="w-[20px] min-[800px]:ml-4  mr-2" src="/bitget-logo.svg" alt=""/><span className="text-sm whitespace-nowrap">Buy on Bitget</span></a>
                                     </div>
                                     <div className="ml-8">
-                                        <div className="text-[#3ACD37] text-5xl font-bold max-[800px]:text-3xl "><span className="mr-1">+</span>2600%</div>
+                                        <div className="text-[#3ACD37] text-5xl font-bold max-[800px]:text-3xl  max-[1250px]:text-[2rem]"><span className="mr-1">+</span>2600%</div>
                                         <div className="text-[7px] text-center text-[#9B9B9B] mt-2">ATH since Launch</div>
                                     </div>
                                 </div>
                             </div>
-                            {/*第一个item*/}
+                            {/*第二个item*/}
                             <a href="https://twitter.com/echomkts">
-                                <div className="flex flex-col items-center bg-black border border-[#737373] rounded-md pt-10 pb-7 max-[800px]:mt-7  text-white min-[799px]:mr-5 max-[1500px]:h-[240px] max-[1300px]:w-[380px] max-[1500px]:w-[400px] w-[480px]">
+                                <div className="max-[1250px]:px-4 flex flex-col items-center bg-black border border-[#737373] rounded-md pt-10 pb-7 max-[800px]:mt-7  text-white min-[799px]:mr-5 max-[1500px]:h-[240px] max-[1250px]:w-[330px]  max-[1300px]:w-[380px] max-[1500px]:w-[400px] max-[1700px]:w-[440px] w-[480px]">
                                     {/*最外面上下布局*/}
                                     <div>
                                         <div className="flex items-center max-[800px]:pr-1 max-[800px]:pl-5">
                                             {/*里面左右布局*/}
                                             <img className="w-[50px]" src="https://ezonline.s3.us-west-2.amazonaws.com/echo_img2.png" alt=""/>
                                             <div className="ml-8 max-[800px]:ml-5">
-                                                <div className="text-4xl max-[1500px]:text-3xl font-bold max-[800px]:text-3xl">ECHO 404</div>
+                                                <div className="text-4xl max-[1500px]:text-3xl font-bold max-[800px]:text-3xl max-[1250px]:text-2xl">ECHO 404</div>
                                                 <div className="text-sm mt-1 max-[800px]:text-xs">The First EOS EVM Smart Inscription (404)</div>
                                             </div>
                                         </div>
                                         <div className="flex items-center mt-7 max-[800px]:ml-5">
                                             <img src="/line1.png" className="h-[80px]" alt=""/>
                                             <div className="ml-3">
-                                                <div className="text-[#3ACD37] text-5xl font-bold max-[800px]:text-3xl"><span className="mr-1">+</span>330%</div>
+                                                <div className="text-[#3ACD37] text-5xl font-bold max-[800px]:text-3xl  max-[1250px]:text-[2rem]"><span className="mr-1">+</span>330%</div>
                                                 <div className="text-[7px] text-center text-[#9B9B9B] mt-2">ATH since Launch</div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </a>
-                            {/*第一个item*/}
+                            {/*第三个item*/}
                             <a href="https://twitter.com/mars404_manta">
-                                <div className="flex flex-col items-center bg-black border border-[#737373] rounded-md pt-10 pb-7 text-white max-[800px]:mt-7 min-[799px]:mr-5  max-[1500px]:h-[240px] max-[1300px]:w-[380px] max-[1500px]:w-[400px] w-[480px]">
+                                <div className="flex flex-col items-center bg-black border border-[#737373] rounded-md pt-10 pb-7 text-white max-[800px]:mt-7 min-[799px]:mr-5  max-[1500px]:h-[240px] max-[1250px]:w-[330px]  max-[1300px]:w-[380px] max-[1500px]:w-[400px] max-[1700px]:w-[440px] w-[480px]">
                                     {/*最外面上下布局*/}
                                     <div>
                                         <div className="flex items-center max-[800px]:pr-1  max-[800px]:pl-5">
                                             {/*里面左右布局*/}
                                             <img className="w-[50px]" src="https://ezonline.s3.us-west-2.amazonaws.com/marsimage.jpeg" alt=""/>
                                             <div className="ml-8 max-[800px]:ml-5">
-                                                <div className="text-4xl max-[1500px]:text-3xl font-bold max-[800px]:text-3xl">Mars 404</div>
+                                                <div className="text-4xl max-[1500px]:text-3xl font-bold max-[800px]:text-3xl max-[1250px]:text-2xl">Mars 404</div>
                                                 <div className="text-sm mt-1 max-[800px]:text-xs">Manta 404</div>
                                             </div>
                                         </div>
                                         <div className="flex items-center mt-7 max-[800px]:ml-5">
                                             <img src="/line2.svg" className="h-[80px]" alt=""/>
                                             <div className="ml-3">
-                                                <div className="text-[#3ACD37] text-5xl font-bold max-[800px]:text-3xl"><span className="mr-1">+</span>4001%</div>
+                                                <div className="text-[#3ACD37] text-5xl font-bold max-[800px]:text-3xl max-[1250px]:text-[2rem]"><span className="mr-1">+</span>4001%</div>
                                                 <div className="text-[7px] text-center text-[#9B9B9B] mt-2">ATH since Launch</div>
                                             </div>
                                         </div>

024ca74cad5d35dba2195eb5a410721bdb5c0c01
coding <1225335417@qq.com>
2024-10-31 20:24:11 +0800

    修改baseurl

--- pages/api/queryTradeAveragePrice.js
+++ pages/api/queryTradeAveragePrice.js
@@ -4,8 +4,8 @@ import nextConfig from '../../next.config.js'
 
 export default async function handler(req, res) {
   try {
-    const response = await axios.post("https://testapi.ezswap.io/tradeActivity/queryTradeAveragePrice", req.body);
-    // const response = await axios.post(BASE_URL + "tradeActivity/queryTradeAveragePrice", req.body);
+    // const response = await axios.post("https://testapi.ezswap.io/tradeActivity/queryTradeAveragePrice", req.body);
+    const response = await axios.post(BASE_URL + "tradeActivity/queryTradeAveragePrice", req.body);
     res.status(200).json(response.data);
   } catch (error) {
     res.status(error.response?.status || 500).json(error.response?.data || {});

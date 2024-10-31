b9c883908c63eddcf0487db7b96612c37d2bb11b
coding <1225335417@qq.com>
2024-10-31 20:30:15 +0800

    修改baseurl

--- pages/api/queryOwnerNFT.js
+++ pages/api/queryOwnerNFT.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
-
+import { BASE_URL } from '../../config/constant';
 export default async function handler(req, res) {
     try {
-        const response = await axios.post("https://testapi.ezswap.io/"+ 'queryOwnerNFT', req.body);
+        const response = await axios.post(BASE_URL+ 'queryOwnerNFT', req.body);
         res.status(200).json(response.data);
     } catch (error) {
         res.status(error.response?.status || 500).json(error.response?.data || {});
